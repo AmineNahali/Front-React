f238d201dcd184f2e70e340a144139aa8830c0c0
SuppRanoo <nahali.amine.32@gmail.com>
2023-07-27 18:37:42 +0100

    added input: field and buttons, of captcha input

--- src/components/CustomCaptcha/CaptchaElement.tsx
+++ src/components/CustomCaptcha/CaptchaElement.tsx
@@ -1,18 +1,29 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from "../../App";
 import loading from "../../assets/robot/loading.gif";
 import "./CustomCaptcha.css"
+import submit from "../../assets/ok.svg"
+import reload from "../../assets/reload.svg"
 
 const CaptchaElement = () => {
 
+    const [captchaCode, setCaptchaCode] = useState("")
     const [image1, setImage1] = useState(loading);
+    const [imageWidth, setImageWidth] = useState("48px");
+    const [imageHeight, setImageHeight] = useState("48px");
+
+    const [showText, setShowText] = useState("block");
+    const [showInput, setShowInput] = useState("none");
 
     const getCaptcha = () => {
         const timer = setTimeout(() => {
-            fetch('http://localhost:3000/captcha/image/' + randomString(),{method:'POST'})
+            fetch('http://localhost:3000/captcha/image/' + randomString(), { method: 'POST' })
                 .then(res => res.blob())
                 .then(data => {
+                    setImageHeight("100px");
+                    setImageWidth("200px");
+                    setShowText("none");
+                    setShowInput("block");
                     setImage1(URL.createObjectURL(data));
                     clearTimeout(timer);
                 })
@@ -22,20 +33,39 @@ const CaptchaElement = () => {
                 })
         }, 2000)
     }
-    
+
     useEffect(() => {
         getCaptcha();
     }, [])
-    
+
 
     return (
         <div className="captchaContainer">
             <table className="loadingTable">
                 <tbody>
+
                     <tr>
-                        <td style={{width:'100%'}}><img className="loading" src={image1}/></td>
+                        <td style={{ width: '100%' }}><img style={{ width: imageWidth, height: imageHeight, margin: "10px" }} className="loading" src={image1} /></td>
                     </tr>
-                    <tr style={{ color: "black" }}><td>Security check, please wait ...</td></tr>
+
+                    <tr style={{ display: showText, color: "black" }}><td>Security check, please wait ...</td></tr>
+
+                    <tr style={{ display: showInput , transform:"scale(0.8)"}}>
+                        <td style={{ height: "32px" }}>
+                            <input type="text" className="captchaInput" placeholder="type the code" value={captchaCode}
+                                onChange={(event) => {
+                                    setCaptchaCode(event.target.value);
+                                }}
+                            />
+                        </td>
+                        <td style={{ height: "32px", width: "32px" }}>
+                            <img className="captchaButton" src={submit} />
+                        </td>
+                        <td style={{ height: "32px", width: "32px" }}>
+                            <img className="captchaButton" src={reload} />
+                        </td>
+                    </tr>
+
                 </tbody>
             </table>
         </div>
--- src/components/CustomCaptcha/CustomCaptcha.tsx
+++ src/components/CustomCaptcha/CustomCaptcha.tsx
@@ -3,26 +3,26 @@ import CaptchaElement from "./CaptchaElement";
 import "./CustomCaptcha.css"
 
 
-const CustomCaptcha = () =>{
-    
+const CustomCaptcha = () => {
+
     const [captchaSize, setCaptchaSize] = useState(0);
     const [captchaOpacity, setCaptchaOpacity] = useState(0);
     const [showAll, setShowAll] = useState(false);
-    setTimeout(()=>{setShowAll(true)},1000);
-    
-    var y:number = setInterval(()=>{
-        if(showAll){
-            if(captchaSize <= 0.9){
-                setCaptchaSize(captchaSize+0.1);
+    setTimeout(() => { setShowAll(true) }, 1000);
+
+    var y = setInterval(() => {
+        if (showAll) {
+            if (captchaSize <= 0.9) {
+                setCaptchaSize(captchaSize + 0.1);
             }
-            if(captchaOpacity < 1){
-                setCaptchaOpacity(captchaOpacity+0.05);
+            if (captchaOpacity < 1) {
+                setCaptchaOpacity(captchaOpacity + 0.05);
             }
-        } 
+        }
         clearInterval(y);
-    },30);
-    return(
-        <div className="CustomCaptcha" style={{transform:"scale("+captchaSize+")",opacity:captchaOpacity}}>
+    }, 30);
+    return (
+        <div className="CustomCaptcha" style={{ transform: "scale(" + captchaSize + ")", opacity: captchaOpacity }}>
             <div className="captchaBox">
                 <CaptchaElement />
             </div>
